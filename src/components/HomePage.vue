5702527446012b15809aedffc645d4222cac0e81
OmgCopito95 <july1995bolt@gmail.com>
2022-06-26 19:33:56 -0300

    crea nuevo componente y llama a helloworld

---
+++ src/components/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1> Hola Mundo </h1>
+
+        <HelloWorldVue />
+    </div>
+
+</template>
+
+<script>
+
+import HelloWorldVue from './HelloWorld.vue'
+export default {
+    name: 'HomePage',
+    components: { // <-- Componentes que se usan en el componente
+        HelloWorldVue,
+    },
+}
+
+</script>
