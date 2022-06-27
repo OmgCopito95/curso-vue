2f0216a5c064dbe412664d554a671bba134f3a68
OmgCopito95 <july1995bolt@gmail.com>
2022-06-27 19:53:01 -0300

    instala vuetify y empieza a generar el componente base

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,20 @@
 <template>
-  <div id="app">
-    <HomePage auth="Holiiis caca"/>
-  </div>
+  <PaginaBase />
 </template>
 
 <script>
-import HomePage from './components/HomePage.vue'
+import PaginaBase from './layout/Base.vue';
+
 
 export default {
-  name: 'App', 
-  components: {
-    HomePage,
-  }
-}
-</script>
+  name: 'App',
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    PaginaBase
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
